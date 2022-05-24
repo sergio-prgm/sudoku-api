a72cbf6397b4bc3d02ba793c6ed848b67d196188
sergio-dev <sergio-dev@outlook.com>
2022-05-24 12:09:15 +0200

    First commit: start server

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const { response } = require('express')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Sudoku API</h1>')
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
